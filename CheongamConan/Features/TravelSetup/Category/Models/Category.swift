c54fbeaa57693cadab2fb93ed36af0c10a3b1829
정홍섭(Theo) <126876363+junghongseop@users.noreply.github.com>
2026-07-13 23:05:01 +0900

    feat: 카테고리 모델 구현

---
+++ CheongamConan/Features/TravelSetup/Category/Models/Category.swift
@@ -0,0 +1,32 @@
+//
+//  Category.swift
+//  CheongamConan
+//
+//  Created by 정홍섭 on 7/13/26.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable, Identifiable, Hashable {
+    case cafe
+    case restaurant
+    case landmark
+    case random
+    
+    var id: Self {
+        self
+    }
+    
+    var title: String {
+        switch self {
+        case .restaurant:
+            "식당"
+        case .cafe:
+            "카페"
+        case .landmark:
+            "명소"
+        case .random:
+            "랜덤"
+        }
+    }
+}
