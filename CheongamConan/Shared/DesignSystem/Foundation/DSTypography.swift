ea889780465a07c6d05ceecf568ff219a5509d9d
LeeDayday <dayoon0905@gmail.com>
2026-07-16 15:54:24 +0900

    chore(#26): Pretendard 폰트 등록 및 타이포그래피 스타일 정의

---
+++ CheongamConan/Shared/DesignSystem/Foundation/DSTypography.swift
@@ -0,0 +1,22 @@
+//
+//  DSTypography.swift
+//  CheongamConan
+//
+//  Created by Dayoon Lee on 7/16/26.
+//
+
+import SwiftUI
+
+enum DSTypography {
+    static let H1: Font = Font.custom("Pretendard-ExtraBold", size: 48)
+    static let H2: Font = Font.custom("Pretendard-ExtraBold", size: 40)
+    static let H3: Font = Font.custom("Pretendard-Bold", size: 36)
+    static let H4: Font = Font.custom("Pretendard-Bold", size: 30)
+
+    static let B1: Font = Font.custom("Pretendard-Bold", size: 24)
+    static let B2: Font = Font.custom("Pretendard-Bold", size: 20)
+    
+    static let C1: Font = Font.custom("Pretendard-SemiBold", size: 16)
+    static let C2: Font = Font.custom("Pretendard-Medium", size: 16)
+    static let C3: Font = Font.custom("Pretendard-Regular", size: 12)
+}
