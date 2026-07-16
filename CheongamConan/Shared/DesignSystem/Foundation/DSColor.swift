014b21f14974c1d54214ecdb2309bba26a6637de
LeeDayday <dayoon0905@gmail.com>
2026-07-16 16:16:53 +0900

    chore(#26): 디자인 시스템 색상 에셋 및 토큰 추가

---
+++ CheongamConan/Shared/DesignSystem/Foundation/DSColor.swift
@@ -0,0 +1,14 @@
+//
+//  DSColor.swift
+//  CheongamConan
+//
+//  Created by Dayoon Lee on 7/16/26.
+//
+
+import SwiftUI
+
+enum DSColor {
+    static let brandPrimary = Color("BrandPrimary")
+    static let brandBlack = Color("BrandBlack")
+    static let brandWhite = Color("BrandWhite")
+}
