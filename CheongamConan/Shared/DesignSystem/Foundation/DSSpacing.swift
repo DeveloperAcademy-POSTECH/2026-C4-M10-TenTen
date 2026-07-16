b83f4a6c6daf7f40ae9c276c6470888036fdc2c3
LeeDayday <dayoon0905@gmail.com>
2026-07-16 23:46:19 +0900

    refactor(#26): spacing 토근 네이밍을 실제 값 기준으로 변경

--- CheongamConan/Shared/DesignSystem/Foundation/DSSpacing.swift
+++ CheongamConan/Shared/DesignSystem/Foundation/DSSpacing.swift
@@ -10,13 +10,13 @@ import CoreGraphics
 // 패딩 배수 (4배수)
 enum DSSpacing {
     // 기본 간격 단위
-    static let x1: CGFloat = 4
-    static let x2: CGFloat = 8
-    static let x3: CGFloat = 12
-    static let x4: CGFloat = 16
-    static let x5: CGFloat = 20
-    static let x6: CGFloat = 24
+    static let spacing4: CGFloat = 4
+    static let spacing8: CGFloat = 8
+    static let spacing12: CGFloat = 12
+    static let spacing16: CGFloat = 16
+    static let spacing20: CGFloat = 20
+    static let spacing24: CGFloat = 24
     
     // 공통 레이아웃 간격
-    static let contentHorizontal: CGFloat = x5
+    static let contentHorizontal: CGFloat = spacing20
 }
