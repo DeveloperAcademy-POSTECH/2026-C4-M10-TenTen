412da1da6d0a29eb093a27a62f57797b211c5f99
LeeDayday <dayoon0905@gmail.com>
2026-07-16 17:47:32 +0900

    chore(#26): 공통 버튼 스타일 및 레이아웃 토큰 추가

---
+++ CheongamConan/Shared/DesignSystem/Foundation/DSSpacing.swift
@@ -0,0 +1,22 @@
+//
+//  DSSpacing.swift
+//  CheongamConan
+//
+//  Created by Dayoon Lee on 7/16/26.
+//
+
+import CoreGraphics
+
+// 패딩 배수 (4배수)
+enum DSSpacing {
+    // 기본 간격 단위
+    static let x1: CGFloat = 4
+    static let x2: CGFloat = 8
+    static let x3: CGFloat = 12
+    static let x4: CGFloat = 16
+    static let x5: CGFloat = 20
+    static let x6: CGFloat = 24
+    
+    // 공통 레이아웃 간격
+    static let contentHorizontal: CGFloat = x5
+}
