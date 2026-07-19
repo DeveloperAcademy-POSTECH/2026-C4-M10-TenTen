3843508ab7cdab1c3a23a1b12014d6eeb1a1f288
정홍섭(Theo) <126876363+junghongseop@users.noreply.github.com>
2026-07-20 02:45:27 +0900

    chore: 초기화 코드 추가

--- CheongamConan/Shared/Models/Place.swift
+++ CheongamConan/Shared/Models/Place.swift
@@ -15,11 +15,29 @@ struct Place: Identifiable, Hashable, Decodable {
     let latitude: Double
     let longitude: Double
     let link: URL?
-
+    
     var id: String {
         "\(name)_\(latitude)_\(longitude)"
     }
-
+    
+    init(
+        name: String,
+        category: String,
+        address: String,
+        roadAddress: String,
+        latitude: Double,
+        longitude: Double,
+        link: URL?
+    ) {
+        self.name = name
+        self.category = category
+        self.address = address
+        self.roadAddress = roadAddress
+        self.latitude = latitude
+        self.longitude = longitude
+        self.link = link
+    }
+    
     private enum CodingKeys: String, CodingKey {
         case title
         case category
@@ -29,32 +47,32 @@ struct Place: Identifiable, Hashable, Decodable {
         case mapX = "mapx"
         case mapY = "mapy"
     }
-
+    
     init(from decoder: Decoder) throws {
         let container = try decoder.container(
             keyedBy: CodingKeys.self
         )
-
+        
         let title = try container.decode(
             String.self,
             forKey: .title
         )
-
+        
         let mapX = try container.decode(
             String.self,
             forKey: .mapX
         )
-
+        
         let mapY = try container.decode(
             String.self,
             forKey: .mapY
         )
-
+        
         let link = try container.decode(
             String.self,
             forKey: .link
         )
-
+        
         guard let rawLongitude = Double(mapX),
               let rawLatitude = Double(mapY) else {
             throw DecodingError.dataCorruptedError(
@@ -63,24 +81,24 @@ struct Place: Identifiable, Hashable, Decodable {
                 debugDescription: "장소 좌표를 변환할 수 없습니다."
             )
         }
-
+        
         self.name = title
-
+        
         self.category = try container.decode(
             String.self,
             forKey: .category
         )
-
+        
         self.address = try container.decode(
             String.self,
             forKey: .address
         )
-
+        
         self.roadAddress = try container.decode(
             String.self,
             forKey: .roadAddress
         )
-
+        
         self.longitude = rawLongitude / 10_000_000
         self.latitude = rawLatitude / 10_000_000
         self.link = link.isEmpty ? nil : URL(string: link)
