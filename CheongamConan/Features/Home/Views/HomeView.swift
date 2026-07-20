f6178c622dccda7e0ea709b9e14bc7c458f7d93c
정홍섭(Theo) <126876363+junghongseop@users.noreply.github.com>
2026-07-21 03:41:34 +0900

    style: 홈화면 UI 작업

--- CheongamConan/Features/Home/Views/HomeView.swift
+++ CheongamConan/Features/Home/Views/HomeView.swift
@@ -10,8 +10,41 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         NavigationStack {
-            NavigationLink(destination: TravelSetupEntryView()) {
-                Text("여행 시작하기")
+            ZStack {
+                Image("Background")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .clipped()
+                    .ignoresSafeArea()
+                
+                VStack(alignment: .leading) {
+                    VStack(alignment: .leading, spacing: DSSpacing.spacing12) {
+                        Text("목적지를 정하는 과정이 복잡하고\n부담스러워서 망설이지 않으셨나요?")
+                            .foregroundStyle(.neutralWhite)
+                            .font(DSTypography.C2)
+                        
+                        Text("NAGAM 으로\n목적지를 추천 받고\n여행을 떠나보세요.")
+                            .foregroundStyle(.neutralWhite)
+                            .font(DSTypography.H3)
+                    }
+                    
+                    Spacer()
+                    
+                    // TODO: 추후 버튼을 공통 컴포넌트로 분리
+                    NavigationLink(destination: TravelSetupEntryView()) {
+                        Text("여행 시작하기")
+                            .padding(.vertical, DSSpacing.spacing16)
+                            .font(DSTypography.B1)
+                            .frame(maxWidth: .infinity)
+                            .background(.main400)
+                            .foregroundStyle(.neutralWhite)
+                            .cornerRadius(DSRadius.standard)
+                    }
+                }
+                .padding(.horizontal, DSSpacing.contentHorizontal)
+                .padding(.bottom, DSSpacing.spacing56)
+                .padding(.top, 193)
             }
         }
     }
