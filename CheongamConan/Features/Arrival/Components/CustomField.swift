cf356919d906c4028038b4c1ccbc5ed085c645a5
정홍섭(Theo) <126876363+junghongseop@users.noreply.github.com>
2026-07-22 02:25:16 +0900

    feat: 커스텀 텍스트 필드 추가

---
+++ CheongamConan/Features/Arrival/Components/CustomField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomField.swift
+//  CheongamConan
+//
+//  Created by 정홍섭 on 7/22/26.
+//
+
+import SwiftUI
+
+struct CustomField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomField()
+}
